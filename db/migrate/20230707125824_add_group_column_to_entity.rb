b16bc801179da1cf160bfa2da85c0eaffb02b4a1
Lornakaboro <lornakaboro@gmail.com>
2023-07-07 16:12:23 +0300

    Add migration files

---
+++ db/migrate/20230707125824_add_group_column_to_entity.rb
@@ -0,0 +1,6 @@
+class AddGroupColumnToEntity < ActiveRecord::Migration[7.0]
+  def change
+    add_column :entities, :group_id, :integer
+
+  end
+end
---
+++ db/migrate/20230707130742_add_icon_data_to_groups.rb
@@ -0,0 +1,4 @@
+class AddIconDataToGroups < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
