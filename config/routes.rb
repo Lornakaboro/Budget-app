665c0c3656c926bf017540f9e84b7b66f7f8174b
Lornakaboro <lornakaboro@gmail.com>
2023-07-07 16:12:44 +0300

    Generate project files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'sessions/destroy'
+  get 'home/index'
+  get 'groups/index'
+  get 'groups/show'
+  get 'groups/new'
+  get 'groups/create'
+  get 'entities/index'
+  get 'entities/show'
+  get 'entities/new'
+  get 'entities/create'
+  get 'entities/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
