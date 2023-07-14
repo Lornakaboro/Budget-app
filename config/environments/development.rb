7c640c3bd3a5adac849b06f70eaa00721dda7f26
Lornakaboro <lornakaboro@gmail.com>
2023-07-14 23:41:01 +0300

    Add views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,10 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -66,5 +70,5 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.disable_request_forgery_protection = trueconfig.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
