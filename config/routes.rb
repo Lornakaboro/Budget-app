8b48a8c4f19964e5e9653ce191eb1e2de082befe
Lornakaboro <lornakaboro@gmail.com>
2023-07-14 23:39:32 +0300

    Add fonts and images

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'sessions/destroy'
-  get 'home/index'
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/new'
-  get 'groups/create'
-  get 'entities/index'
-  get 'entities/show'
-  get 'entities/new'
-  get 'entities/create'
-  get 'entities/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root to: 'home#index'
+  resources :users, only: [:index, :show, :new] do
+    resources :groups, only: [:new, :create, :show, :index, :destroy]
+    resources :entities, only: [:new, :create, :index, :show, :destroy]
+  end
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get 'logout', to: 'session#destroy', as: 'logout'
 end
