115e44917ffda1793a15021e338ee03ff04726de
Lornakaboro <lornakaboro@gmail.com>
2023-07-15 22:19:55 +0300

    Fix server error

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,8 +8,13 @@ class Group < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, too_short: 'Group name must be more than 3 characters long' }
 
   def icon_url
-    return unless icon.attached?
-
-    Rails.application.routes.url_helpers.url_for(icon)
+    if icon.attached?
+      Rails.application.routes.url_helpers.url_for(icon)
+    else
+      # Return the URL of a default icon image or a placeholder image
+      # if no icon is attached to the group.
+      # Example:
+      ActionController::Base.helpers.asset_path('default_icon.png')
+    end
   end
 end
