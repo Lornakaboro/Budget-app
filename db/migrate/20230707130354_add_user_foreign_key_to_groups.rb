8e63b495c231d187a4a8efc6799527521526fb4e
Lornakaboro <lornakaboro@gmail.com>
2023-07-15 16:28:57 +0300

    Installed missing gems

--- db/migrate/20230707130354_add_user_foreign_key_to_groups.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddIconDataToGroups < ActiveRecord::Migration[7.0]
-  def change
-    add_column :groups, :icon_data, :text
-  end
-end
--- db/migrate/20230707130742_add_icon_data_to_groups.rb
+++ db/migrate/20230707130742_add_icon_data_to_groups.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-
 class AddIconDataToGroups < ActiveRecord::Migration[7.0]
-  def change; end
+  def change
+    add_column :groups, :icon_data, :text
+  end
 end
