808da35b75691ac5a0aa0c646d21eb51e6177ce7
Lornakaboro <lornakaboro@gmail.com>
2023-07-15 16:58:05 +0300

    Modify migrations to have pssing tests

---
+++ db/migrate/20230715134810_add_foreign_key_constraint_to_groups.rb
@@ -0,0 +1,6 @@
+class AddForeignKeyConstraintToGroups < ActiveRecord::Migration[7.0]
+  def change 
+    add_reference :groups, :author, null: false, foreign_key: { to_table: :users }
+    
+  end
+end
